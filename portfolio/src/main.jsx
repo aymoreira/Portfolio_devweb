3ac9b6d9df605dfb72f70fe355346417cd61ba7f
Aymeric Moreira <aymeric.moreira@efrei.net>
2026-05-20 16:00:08 +0200

    supression du template de base

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import "@fontsource/*";
+
+import "@fontsource/comfortaa"
+import "@fontsource/raleway"
+import "@fontsource/nunito"
+import "@fontsource/poppins"
+import "@fontsource/roboto"
+import "@fontsource/playfair-display"
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
